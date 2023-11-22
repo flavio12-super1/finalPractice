a4ee147d367df9647efad7fe2be9c011fdf04c85
hacker-flavio <theflavioherrera@gmail.com>
2023-11-22 13:52:41 -0800

    array

--- arrayListProjects/project1/ArrayList.h
+++ arrayListProjects/project1/ArrayList.h
@@ -2,29 +2,47 @@
 #define ARRAY_LIST_H
 
 #include <iostream>
-#include <ostream>
-#include <stdexcept>
 
-using namespace std;
-
+template <class T>
 class ArrayList {
-    private:
-    public:
-        int capacity;
-        int count;
+private:
 
-        int * arr;
+public:
+    int capacity;
+    int count;
+    T* arr;
 
-        ArrayList(){
-            capacity = 1;
-            count = 0;
+    ArrayList() {
+        capacity = 1;
+        count = 0;
+        arr = new T[capacity];
+    }
 
-            arr = new int[capacity];
-        }
+    // ~ArrayList() {
+    //     for (int i = 0; i < count; i++){
+    //         for (int j = 0; j < arr[i].count; j++){
+    //         //do stuff
+    //         }      
+    //     }
+    // }
 
-        ~ArrayList(){
+    void inflate() {
+        if (count == capacity) {
+            capacity *= 2;
+            T* newArr = new T[capacity];
+            for (int i = 0; i < count; ++i) {
+                newArr[i] = arr[i];
+            }
             delete[] arr;
+            arr = newArr;
         }
+    }
+
+    void append(T value) {
+        inflate();
+        arr[count] = value;
+        count++;
+    }
 };
 
 #endif
--- arrayListProjects/project1/main.cpp
+++ arrayListProjects/project1/main.cpp
@@ -5,10 +5,23 @@
 
 using namespace std;
 
-int main(){
+int main() {
+    ArrayList< ArrayList<int> > myIntArrayList;
+    myIntArrayList.append(ArrayList<int>());
+    myIntArrayList.append(ArrayList<int>());
+    myIntArrayList.append(ArrayList<int>());
 
 
-    ArrayList myArray;
-    cout << myArray.capacity << endl;
+    // Append some elements to the inner ArrayList<int>
+    myIntArrayList.arr[0].append(42);
+    myIntArrayList.arr[0].append(10);
+    myIntArrayList.arr[2].append(69);
+
+
+    // Access elements
+    std::cout << myIntArrayList.arr[0].arr[0] << std::endl;  // Output: 42
+    std::cout << myIntArrayList.arr[0].arr[1] << std::endl;  // Output: 10
+
+    std::cout << myIntArrayList.arr[2].arr[0] << std::endl;  // Output: 10
     return 0;
 }
